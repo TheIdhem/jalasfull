6653e5ad1e6328dca93950aa558d8cc571960832
mehdifallahi <mohammadf00900@gmail.com>
2019-11-29 13:29:51 +0330

    initial commit

---
+++ src/main/kotlin/ir/faj/jalas/jalas/JalasApplication.kt
@@ -0,0 +1,11 @@
+package ir.faj.jalas.jalas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JalasApplication
+
+fun main(args: Array<String>) {
+    runApplication<JalasApplication>(*args)
+}
---
+++ src/test/kotlin/ir/faj/jalas/jalas/JalasApplicationTests.kt
@@ -0,0 +1,13 @@
+package ir.faj.jalas.jalas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JalasApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
