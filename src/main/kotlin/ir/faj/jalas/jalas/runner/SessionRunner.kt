5140f0aa17eeb4716321fec482e2b47cbd8c2010
mehdifallahi <mohammadf00900@gmail.com>
2019-11-30 11:46:20 +0330

    add command line runner

---
+++ src/main/kotlin/ir/faj/jalas/jalas/runner/SessionRunner.kt
@@ -0,0 +1,12 @@
+package ir.faj.jalas.jalas.runner
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class SessionRunner : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+
+    }
+}
